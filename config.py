68113e878adf10d536d7d98d8c46bce447e6dd03
kunci115 <rino.alpin@gmail.com>
2022-11-11 15:39:10 +0700

    [add] Telegram chat fetching coinmarketcap

---
+++ config.py
@@ -0,0 +1,68 @@
+from emoji import emojize
+from aiogram.utils.markdown import pre
+from dotenv import load_dotenv, dotenv_values
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import os
+
+
+os.environ['TELEGRAM_BOT_KEY'] = dotenv_values('.env')['TELEGRAM_BOT_KEY']
+os.environ['COINMARKETCAP_API_KEY'] = dotenv_values('.env')['COINMARKETCAP_API_KEY']
+TELEGRAM_API_TOKEN = os.getenv('TELEGRAM_BOT_KEY')
+COINMARKETCAP_API_TOKEN = os.getenv('COINMARKETCAP_API_KEY')
+
+
+class BUTTON:
+    IDR = ['Convert to IDR', 'Please convert to idr', 'fetch idr price']
+    USD = ['Convert to USD', 'Please conver to usd', 'fetch to usd']
+
+
+class MESSAGE:
+    START = "Welcome to fetcher price bot, right now this bot only cover btc, eth to idr or usd to fetch"
+
+
+class URL:
+
+    class COINMARKETCAP:
+        NAME = 'coinmarketcap'
+        API = 'https://pro-api.coinmarketcap.com/v1'
+
+
+class CURRENCY_FROM_IDR:
+    BTC = 'BTC TO IDR'
+    ETH = 'ETH TO IDR'
+
+
+class CURRENCY_FROM_USD:
+    BTC = 'BTC TO USD'
+    ETH = 'ETH TO USD'
+
+
+class KeyboardsBot(object):
+
+    def __init__(self):
+        self.btn_idr = KeyboardButton(BUTTON.IDR[0])
+        self.btn_usd = KeyboardButton(BUTTON.USD[0])
+        self.btn = ReplyKeyboardMarkup(
+            resize_keyboard=True
+        ).row(self.btn_idr, self.btn_usd)
+
+
+class KeyboardsCurrencyIDRBot(object):
+    def __init__(self):
+        self.btn_btc = KeyboardButton(CURRENCY_FROM_IDR.BTC)
+        self.btn_eth = KeyboardButton(CURRENCY_FROM_IDR.ETH)
+        self.btn = ReplyKeyboardMarkup(
+            resize_keyboard=True
+        ).row(self.btn_btc, self.btn_eth)
+
+
+class KeyboardsCurrencyUSDBot(object):
+    def __init__(self):
+        self.btn_btc = KeyboardButton(CURRENCY_FROM_USD.BTC)
+        self.btn_eth = KeyboardButton(CURRENCY_FROM_USD.ETH)
+        self.btn = ReplyKeyboardMarkup(
+            resize_keyboard=True
+        ).row(self.btn_btc, self.btn_eth)
+
+
+
---
+++ main.py
@@ -0,0 +1,92 @@
+import json
+import math
+import logging
+
+from requests import Session
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from aiogram.types import ParseMode
+from config import TELEGRAM_API_TOKEN, COINMARKETCAP_API_TOKEN, \
+    BUTTON, MESSAGE, URL, KeyboardsBot, KeyboardsCurrencyIDRBot, CURRENCY_FROM_IDR,\
+    CURRENCY_FROM_USD, KeyboardsCurrencyUSDBot
+
+
+async def getInfo(from_c, to_c):  # Function to get the info
+
+    url = URL.COINMARKETCAP.API + "/cryptocurrency/quotes/latest"  # Coinmarketcap API url
+
+    parameters = {'slug': from_c, # ex:bitcoin
+                  'convert': to_c}  # ex:USD
+
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': COINMARKETCAP_API_TOKEN
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+    response = session.get(url, params=parameters)
+
+    info = json.loads(response.text)
+
+    # logging.info(info['data']['1']['quote'])
+    logging.info(info['data'])
+    return info['data']
+
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(token=TELEGRAM_API_TOKEN)
+dp = Dispatcher(bot)
+kb = KeyboardsBot()
+kcb_idr = KeyboardsCurrencyIDRBot()
+kcb_usd = KeyboardsCurrencyUSDBot()
+
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply(MESSAGE.START, reply_markup=kb.btn)
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    if msg.text in ["hi", "hallo", "hello", "halo"]:
+        await msg.reply(MESSAGE.START, reply_markup=kb.btn)
+    elif msg.text in ["no", "no thanks", "thanks", "bye"]:
+        await msg.reply("Alright bye, Thanks")
+    elif msg.text in BUTTON.IDR:
+        await msg.reply("Alright, you want to fetch into IDR from which currency?",
+                        reply_markup=kcb_idr.btn, parse_mode=ParseMode.MARKDOWN)
+    elif msg.text == BUTTON.USD:
+        await msg.reply("Alright, you want to fetch into USD from which currency?",
+                        reply_markup=kcb_usd.btn, parse_mode=ParseMode.MARKDOWN)
+    elif msg.text == CURRENCY_FROM_IDR.BTC:
+        mes = await getInfo(from_c="bitcoin", to_c="IDR")
+        idr_price = math.ceil(mes['1']['quote']['IDR']['price'])
+        message = "1 Bitcoin is equal to RP."+str(idr_price)
+        await msg.reply(message)
+        await msg.reply("is there anything you want to do?")
+    elif msg.text == CURRENCY_FROM_IDR.ETH:
+        mes = await getInfo(from_c="ethereum", to_c="IDR")
+        idr_price = math.ceil(mes['1027']['quote']['IDR']['price'])
+        message = "1 Ethereum is equal to RP."+str(idr_price)
+        await msg.reply(message)
+        await msg.reply("is there anything you want to do?")
+    elif msg.text == CURRENCY_FROM_USD.BTC:
+        mes = await getInfo(from_c="bitcoin", to_c="USD")
+        idr_price = math.ceil(mes['1']['quote']['USD']['price'])
+        message = "1 Bitcoin is equal to $" + str(idr_price)
+        await msg.reply(message)
+        await msg.reply("is there anything you want to do?")
+    elif msg.text == CURRENCY_FROM_USD.ETH:
+        mes = await getInfo(from_c="ethereum", to_c="USD")
+        idr_price = math.ceil(mes['1027']['quote']['USD']['price'])
+        message = "1 Ethereum is equal to $" + str(idr_price)
+        await msg.reply(message)
+        await msg.reply("is there anything you want to do?")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
